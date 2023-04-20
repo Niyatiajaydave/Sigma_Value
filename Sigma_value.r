8961f1340fc9d949e28e860966bbb6e79e9f43dc
Niyatiajaydave <79849440+Niyatiajaydave@users.noreply.github.com>
2023-04-20 12:37:36 +0200

    Add files via upload
    Initial

---
+++ Sigma_value.r
@@ -0,0 +1,27 @@
+
+R version 3.2.3 (2015-12-10) -- "Wooden Christmas-Tree"
+Copyright (C) 2015 The R Foundation for Statistical Computing
+Platform: i386-w64-mingw32/i386 (32-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[Previously saved workspace restored]
+
+> sig=c(1,2,1,3,2,13,14,9,1,14,21,9,3,9,9,15)
+> mu=c(1,5,8,2)
+>  sig=matrix(sig,nrow=4,byrow=TRUE)
+> det(sig)
+[1] 144
+> 
